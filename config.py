cd79ca2e9d26e27c7216dc5f094cefe84fe9b345
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2019-08-10 00:15:29 +0300

    Add files via upload
    Class, which parse articles from site lenta.ru without parsing modules.
    Three txt have results of work.

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+# for lenta.ru
 url = 'https://lenta.ru/news/2019/07/24/shi/'
+# url = 'https://lenta.ru/news/2019/08/06/otdykh/'
+# url = 'https://lenta.ru/news/2019/08/06/zerkalo/'
 title_start_tag = '<h1 class="b-topic__title" itemprop="headline">'
 title_finish_tag = '</h1><div class="b-topic__socials _header">'
 text_start_tag = '<div class="b-text clearfix js-topic__text" itemprop="articleBody">'
 text_finish_tag = '</p><div class="b-box">'
+
--- reading_articles.py
+++ reading_articles.py
@@ -2,48 +2,102 @@ import urllib.request
 import re
 from config import *
 
-def extract_article(url):
+class Extract_article():
     """
-    Function, which extract article from web page
+    Class, which extract article from web page (only lenta.ru now)
     :param url:
     :return:
     """
-    # read page
-    response = urllib.request.urlopen(url)
-    article = response.read().decode('utf-8')
+    def __init__(self):
+        self.url = url
+        self.title_start_tag = title_start_tag
+        self.title_finish_tag = title_finish_tag
+        self.text_start_tag = text_start_tag
+        self.text_finish_tag = text_finish_tag
+        self.article = ''
+        self.title = ''
+        self.text = ''
+        self.words = []
+        self.publication = ''
 
-    # get title of article
-    title = article.split(title_start_tag)[1].split(title_finish_tag)[0].replace('&nbsp;', ' ')
 
-    # get text of article
-    text = article.split(text_start_tag)[1].split(text_finish_tag)[0]
-    text = text.replace('<p>', '')
-    text = text.replace('</p>', '\n\n')
-    text = text.replace('<i>', '').replace('</i>', '')
-    text = text.replace('<a href="', '[').replace('" target="_blank">', '] ').replace('</a>', '')
-    words = text.split(' ')
+    def read_page(self):
+        """
+        Function which get html from page
+        :return:
+        """
+        response = urllib.request.urlopen(self.url)
+        self.article = response.read().decode('utf-8')
 
-    # purify text
-    for i in words:
-        if i.startswith('['):
-            if re.search(r'(https?://[^\s]+)', i):
-                pass
+
+    def get_title(self):
+        """
+        Function, which get title text
+        :return:
+        """
+        self.title = self.article.split(title_start_tag)[1].split(title_finish_tag)[0].replace('&nbsp;', ' ')
+
+    def get_text(self):
+        """
+        Function, which get text
+        :return:
+        """
+        self.text = self.article.split(text_start_tag)[1].split(text_finish_tag)[0]
+        self.text = self.text.replace('<p>', '')
+        self.text = self.text.replace('</p>', '\n\n')
+        self.text = self.text.replace('<i>', '').replace('</i>', '')
+        self.text = self.text.replace('<a href="', '[').replace('" target="_blank">', '] ').replace('</a>', '')
+        self.words = self.text.split(' ')
+
+    def purify_text(self):
+        """
+        Clean text from local lincs
+        :return:
+        """
+        for i in self.words:
+            if i.startswith('['):
+                if re.search(r'(https?://[^\s]+)', i):
+                    pass
+                else:
+                    self.words.remove(i)
+
+    def format_text(self):
+        """
+        Format text
+        :return:
+        """
+        self.publication = '' + self.title + '\n\n'
+        line = ''
+        for word in self.words:
+            if (len(line) + len(word)) >= 81:
+                self.publication += line + '\n'
+                line = ''
+                line += word + ' '
             else:
-                words.remove(i)
+                line += word + ' '
+        self.publication += line + '\n'
 
-    #
-    publication = '' + title + '\n\n'
-    line = ''
-    for word in words:
-        if (len(line) + len(word)) >= 81:
-            publication += line + '\n'
-            line = ''
-            line += word + ' '
-        else:
-            line += word + ' '
 
-    # write text in .txt
-    with open('{}.txt'.format(title), 'w') as output_file:
-        output_file.write(publication)
+    def write_text(self):
+        """
+        Function, which write text to txt-file
+        :return:
+        """
+        with open('{}.txt'.format(self.title), 'w') as output_file:
+            output_file.write(self.publication)
 
-extract_article(url)
+    def parse(self):
+        """
+        Function, which parse web-page
+        :return:
+        """
+        self.read_page()
+        self.get_title()
+        self.get_text()
+        self.purify_text()
+        self.format_text()
+        self.write_text()
+
+if __name__ == '__main__':
+    parser = Extract_article()
+    parser.parse()
